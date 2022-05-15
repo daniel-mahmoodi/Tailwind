5aec71b2f3807a58412d57ca51b5e3609bf820e4
unknown <mahmoodidanial2@gmail.com>
2022-05-15 19:24:01 +0430

    first practice

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import { useRef, useState } from "react";
+
+function App() {
+  const [error, setError] = useState(false);
+  const passwordValueRef = useRef();
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("useRef", passwordValueRef.current.value);
+    const passwordValue = passwordValueRef.current.value;
+    if (passwordValue) {
+      setError(false);
+    } else {
+      setError(true);
+    }
+    passwordValueRef.current.value = "";
+  };
+
+  return (
+    <div>
+      <h1>test form with useRef</h1>
+      <div className="w-full max-w-xs mt-6">
+        <form
+          onSubmit={formSubmitHandler}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+              ref={passwordValueRef}
+            />
+            {error && (
+              <p className="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            )}
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs">
+          &copy;2020 Acme Corp. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
